a7c758d2c967ffb5ec853dd4f839f1762ff2a20b
João Felipe Zini <joaofelipezini@gmail.com>
2023-09-21 21:56:50 -0300

    req 12 tests

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -6,6 +6,7 @@ import { app } from '../app';
 import UserModel from '../database/models/UserModel';
 import { invalidErrorMsg, missingErrorMsg, mockValidUser } from './mocks/user.mocks';
 import * as bcrypt from 'bcryptjs';
+import Token from '../auth/Token';
 
 const { expect } = chai;
 
@@ -16,74 +17,26 @@ describe('Route /login', function () {
     sinon.restore();
   });
 
-  it('should return a token', async function () {
-    sinon.stub(UserModel, 'findOne').resolves(UserModel.build(mockValidUser));
-    sinon.stub(bcrypt, 'compareSync').returns(true);
+  describe('POST /login', function () {
+    it('should return a token', async function () {
+      sinon.stub(UserModel, 'findOne').resolves(UserModel.build(mockValidUser));
+      sinon.stub(bcrypt, 'compareSync').returns(true);
 
-    const response = await chai.request(app).post('/login').send({
-      email: 'user@user.com',
-      password: '12345678',
-    });
-
-    expect(response).to.have.status(200);
-    expect(response.body).to.have.property('token');
-  });
-
-  it('should return status 401 if user is not found in db', async function () {
-    sinon.stub(UserModel, 'findOne').resolves(null);
-    sinon.stub(bcrypt, 'compareSync').returns(true);
-
-    const response = await chai.request(app).post('/login').send({
-      email: 'user@user.com',
-      password: '12345678',
-    });
-
-    expect(response).to.have.status(401);
-    expect(response.body).to.deep.equal(invalidErrorMsg);
-  });
-
-  it("should return status 401 if password doesn't match", async function () {
-    sinon.stub(UserModel, 'findOne').resolves(UserModel.build(mockValidUser));
-    sinon.stub(bcrypt, 'compareSync').returns(false);
-
-    const response = await chai.request(app).post('/login').send({
-      email: 'user@user.com',
-      password: '12345678',
-    });
-
-    expect(response).to.have.status(401);
-    expect(response.body).to.deep.equal(invalidErrorMsg);
-  });
-
-  it('should return status 401 if password has less than 6 characters', async function () {
-    sinon.stub(UserModel, 'findOne').resolves(UserModel.build(mockValidUser));
-    sinon.stub(bcrypt, 'compareSync').returns(true);
-
-    const response = await chai.request(app).post('/login').send({
-      email: 'user@user.com',
-      password: '12345',
-    });
-
-    expect(response).to.have.status(401);
-    expect(response.body).to.deep.equal(invalidErrorMsg);
-  });
-
-  it("should return status 401 if email doesn't match the regex", async function () {
-    sinon.stub(UserModel, 'findOne').resolves(UserModel.build(mockValidUser));
-    sinon.stub(bcrypt, 'compareSync').returns(true);
-
-    const invalidEmails = [
-      'user.com',
-      'user@user',
-      'user@user.',
-      'user@.com',
-      'user@.',
-      '@user.com',
-    ];
-
-    const testsPromises = invalidEmails.map(async (email) => {
       const response = await chai.request(app).post('/login').send({
-        email,
+        email: 'user@user.com',
+        password: '12345678',
+      });
+
+      expect(response).to.have.status(200);
+      expect(response.body).to.have.property('token');
+    });
+
+    it('should return status 401 if user is not found in db', async function () {
+      sinon.stub(UserModel, 'findOne').resolves(null);
+      sinon.stub(bcrypt, 'compareSync').returns(true);
+
+      const response = await chai.request(app).post('/login').send({
+        email: 'user@user.com',
         password: '12345678',
       });
 
@@ -91,30 +44,110 @@ describe('Route /login', function () {
       expect(response.body).to.deep.equal(invalidErrorMsg);
     });
 
-    await Promise.all(testsPromises);
-  });
+    it("should return status 401 if password doesn't match", async function () {
+      sinon.stub(UserModel, 'findOne').resolves(UserModel.build(mockValidUser));
+      sinon.stub(bcrypt, 'compareSync').returns(false);
 
-  it("should return status 400 if password wasn't sent", async function () {
-    sinon.stub(UserModel, 'findOne').resolves(UserModel.build(mockValidUser));
-    sinon.stub(bcrypt, 'compareSync').returns(true);
+      const response = await chai.request(app).post('/login').send({
+        email: 'user@user.com',
+        password: '12345678',
+      });
 
-    const response = await chai.request(app).post('/login').send({
-      email: 'user@user.com',
+      expect(response).to.have.status(401);
+      expect(response.body).to.deep.equal(invalidErrorMsg);
     });
 
-    expect(response).to.have.status(400);
-    expect(response.body).to.deep.equal(missingErrorMsg);
-  });
+    it('should return status 401 if password has less than 6 characters', async function () {
+      sinon.stub(UserModel, 'findOne').resolves(UserModel.build(mockValidUser));
+      sinon.stub(bcrypt, 'compareSync').returns(true);
 
-  it("should return status 400 if email wasn't sent", async function () {
-    sinon.stub(UserModel, 'findOne').resolves(UserModel.build(mockValidUser));
-    sinon.stub(bcrypt, 'compareSync').returns(true);
+      const response = await chai.request(app).post('/login').send({
+        email: 'user@user.com',
+        password: '12345',
+      });
 
-    const response = await chai.request(app).post('/login').send({
-      password: '12345678',
+      expect(response).to.have.status(401);
+      expect(response.body).to.deep.equal(invalidErrorMsg);
     });
 
-    expect(response).to.have.status(400);
-    expect(response.body).to.deep.equal(missingErrorMsg);
+    it("should return status 401 if email doesn't match the regex", async function () {
+      sinon.stub(UserModel, 'findOne').resolves(UserModel.build(mockValidUser));
+      sinon.stub(bcrypt, 'compareSync').returns(true);
+
+      const invalidEmails = [
+        'user.com',
+        'user@user',
+        'user@user.',
+        'user@.com',
+        'user@.',
+        '@user.com',
+      ];
+
+      const testsPromises = invalidEmails.map(async (email) => {
+        const response = await chai.request(app).post('/login').send({
+          email,
+          password: '12345678',
+        });
+
+        expect(response).to.have.status(401);
+        expect(response.body).to.deep.equal(invalidErrorMsg);
+      });
+
+      await Promise.all(testsPromises);
+    });
+
+    it("should return status 400 if password wasn't sent", async function () {
+      sinon.stub(UserModel, 'findOne').resolves(UserModel.build(mockValidUser));
+      sinon.stub(bcrypt, 'compareSync').returns(true);
+
+      const response = await chai.request(app).post('/login').send({
+        email: 'user@user.com',
+      });
+
+      expect(response).to.have.status(400);
+      expect(response.body).to.deep.equal(missingErrorMsg);
+    });
+
+    it("should return status 400 if email wasn't sent", async function () {
+      sinon.stub(UserModel, 'findOne').resolves(UserModel.build(mockValidUser));
+      sinon.stub(bcrypt, 'compareSync').returns(true);
+
+      const response = await chai.request(app).post('/login').send({
+        password: '12345678',
+      });
+
+      expect(response).to.have.status(400);
+      expect(response.body).to.deep.equal(missingErrorMsg);
+    });
+  });
+
+  describe('GET /login/role', function () {
+    it('should return the user role', async function () {
+      const token = new Token().generateToken(mockValidUser);
+      const response = await chai
+        .request(app)
+        .get('/login/role')
+        .set('Authorization', `Bearer ${token}`);
+
+      expect(response).to.have.status(200);
+      expect(response.body).to.deep.equal({ role: 'admin' });
+    });
+
+    it("should return status 401 if token wasn't sent", async function () {
+      const response = await chai.request(app).get('/login/role');
+
+      expect(response).to.have.status(401);
+      expect(response.body).to.deep.equal({ message: 'Token not found' });
+    });
+
+    it('should return status 401 is token is invalid', async function () {
+      const response = await chai
+        .request(app)
+        .get('/login/role')
+        .set('Authorization', `Bearer thisisaninvalidtoken`);
+
+      expect(response).to.have.status(401);
+      expect(response.body).to.deep.equal({ message: 'Token must be a valid token' });
+    });
   });
 });
--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,7 +1,7 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
-import chaiHttp  = require('chai-http');
+import chaiHttp = require('chai-http');
 import { app } from '../app';
 import TeamModel from '../database/models/TeamModel';
 import { allTeamsMock } from './mocks/teams.mocks';
@@ -14,31 +14,33 @@ describe('Route /teams', function () {
   afterEach(function () {
     sinon.restore();
   });
-  
-  it('should return a list of teams', async function () {
-    sinon.stub(TeamModel, 'findAll').resolves(TeamModel.bulkBuild(allTeamsMock));
 
-    const response = await chai.request(app).get('/teams');
+  describe('GET /teams', function () {
+    it('should return a list of teams', async function () {
+      sinon.stub(TeamModel, 'findAll').resolves(TeamModel.bulkBuild(allTeamsMock));
 
-    expect(response.status).to.be.equal(200);
-    expect(response.body).to.deep.equal(allTeamsMock);
+      const response = await chai.request(app).get('/teams');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal(allTeamsMock);
+    });
+
+    it('should return a team by id', async function () {
+      sinon.stub(TeamModel, 'findByPk').resolves(TeamModel.build(allTeamsMock[0]));
+
+      const response = await chai.request(app).get('/teams/1');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal(allTeamsMock[0]);
+    });
+
+    it("should return status 404 if team doesn't exist in the database", async function () {
+      sinon.stub(TeamModel, 'findByPk').resolves(null);
+
+      const response = await chai.request(app).get('/teams/1');
+
+      expect(response.status).to.be.equal(404);
+      expect(response.body).to.deep.equal({ message: 'No team was found' });
+    });
   });
-
-  it('should return a team by id', async function () {
-    sinon.stub(TeamModel, 'findByPk').resolves(TeamModel.build(allTeamsMock[0]));
-
-    const response = await chai.request(app).get('/teams/1');
-
-    expect(response.status).to.be.equal(200);
-    expect(response.body).to.deep.equal(allTeamsMock[0]);
-  });
-
-  it('should return status 404 if team doesn\'t exist in the database', async function () {
-    sinon.stub(TeamModel, 'findByPk').resolves(null);
-
-    const response = await chai.request(app).get('/teams/1');
-
-    expect(response.status).to.be.equal(404);
-    expect(response.body).to.deep.equal({ message: 'No team was found' });
-  });
-})
+});
